3f53dcff640ca0c1b7376b069fb6759c6e99614a
amanver02 <developeraman02@gmail.com>
2024-07-12 19:07:53 +0530

    #

---
+++ Basics/jyant.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;//declaration
+    x=5;//initialization   
+    cout<< x ;
+    cout <<endl;
+    cout<<x + 6;
+    int y = 9 ;cout<<endl;
+    cout<<x*y;
+}
